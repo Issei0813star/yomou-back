4fda8aa81d77c4d69a888ffcfd5ae82cb6d0c366
Issei0813star <issei0813star.dev@gmail.com>
2024-08-17 22:46:30 +0900

    認証コード管理に排他制御を実装

--- src/main/java/com/yomou/tempstorage/manager/TempVerificationCodeManager.java
+++ src/main/java/com/yomou/tempstorage/manager/TempVerificationCodeManager.java
@@ -7,14 +7,13 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @RequiredArgsConstructor
 public class TempVerificationCodeManager {
-    //Map<userId, 認証コード>
+    // Map<userId, 認証コード>
     private final ConcurrentHashMap<Long, VerificationCodeDto> tempUserVerificationCodeMap = new ConcurrentHashMap<>();
 
     /**
@@ -22,11 +21,10 @@ public class TempVerificationCodeManager {
      * @param userId Long
      * @return String
      */
-    public String getVerificationCode(Long userId) {
+    public synchronized String getVerificationCode(Long userId) {
         String verificationCode = this.createVerificationCode();
         VerificationCodeDto dto = new VerificationCodeDto(verificationCode, this.getExpiration());
         this.tempUserVerificationCodeMap.put(userId, dto);
-
         return verificationCode;
     }
 
@@ -36,22 +34,15 @@ public class TempVerificationCodeManager {
      * @param userId Long
      * @return boolean
      */
-    public boolean verifyCode(String verificationCode, Long userId) {
+    public synchronized boolean verifyCode(String verificationCode, Long userId) {
         VerificationCodeDto dto = this.tempUserVerificationCodeMap.get(userId);
-        if(Objects.isNull(dto)) {
+        if (dto == null) {
             return false;
         }
-
-        Instant expiration = dto.getExpiration();
-
-        if(Objects.isNull(expiration)) {
-            return false;
-        }
-
-        return expiration.isAfter(Instant.now());
+        return verificationCode.equals(dto.getVerificationCode()) && dto.getExpiration().isAfter(Instant.now());
     }
 
-    public void removeCode(Long userId) {
+    public synchronized void removeCode(Long userId) {
         this.tempUserVerificationCodeMap.remove(userId);
     }
 
@@ -61,13 +52,13 @@ public class TempVerificationCodeManager {
         return String.valueOf(code);
     }
 
-    private Instant getExpiration () {
+    private Instant getExpiration() {
         Instant now = Instant.now();
         return now.plus(Duration.ofMinutes(30));
     }
 
     @Scheduled(fixedRate = 1800000)
-    private void removeCodeScheduled() {
+    private synchronized void removeCodeScheduled() {
         Instant now = Instant.now();
         this.tempUserVerificationCodeMap.entrySet().removeIf(entry -> now.isAfter(entry.getValue().getExpiration()));
     }
